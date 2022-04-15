0ab084a4b1838f1e91e99301fe5fea2a4ca3e326
Proteseus <horriblebob11@gmail.com>
2022-04-16 01:19:51 +0300

    Revert "heroku 15-4-2022 update"
    This reverts commit 7281353dfe8221cea48a03812312ea3ff41d9dad.

--- TrendingNewsMenu.py
+++ TrendingNewsMenu.py
@@ -35,9 +35,8 @@ def dwn():
 
 def res():
     with open('trendingCount.txt','w') as d:
-        if(d.write('-1')):
-            print('count reset...')
-        
+                d.write('-1')
+
 def news_printer(f):
     with open('trending_news.json', 'r') as St:
         my_dict = json.load(St)
@@ -65,9 +64,6 @@ def menu_picker():
 
 ######menus######
 def start_trend_option(update, context):
-    with open('trendingCount.txt', 'r') as trc:
-        tr = trc.read()
-        print(tr)
     res()
     update.message.reply_text(main_menu_message(),
                             reply_markup=main_menu_keyboard())
@@ -121,7 +117,6 @@ def last_menu_keyboard():
     return InlineKeyboardMarkup(keyboard)
 
 #####message######
-#print interactive message
 def main_menu_message():
     TN.get_News()
     return 'Trending News'
@@ -133,6 +128,7 @@ def first_menu_message():
         n = upp()
     else:
         n = int(f)
+
     strT = news_printer(n)
     print(strT)
     return strT
--- starter.py
+++ starter.py
@@ -1,26 +1,11 @@
 def starter():
     with open('status.txt', 'w') as sts:
-        if(sts.write('.')):
-            print('stat ready...')
-
-    with open('status.txt', 'r') as sts:
-        st = sts.read()
-        print (st)
+        sts.write('.')
 
     with open('trendingCount.txt', 'w') as trc:
-        if(trc.write('-1')):
-            print('count initiated...')
-    
-    with open('trendingCount.txt', 'r') as trc:
-        tr = trc.read()
-        print(tr)
+        trc.write('-1')
 
-    with open("bookStatus.txt", 'w') as status:
-        if(status.write("not_book")):
-            print('books made...')
+    with open("status.txt", 'w') as status:
+        status.write("not_book")
     
-    with open("bookStatus.txt", 'r') as status:
-        st = status.read()
-        print(st)
-
     print("Kickoff...")
